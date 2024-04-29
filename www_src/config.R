348b5ade25d979976d71e69518f11ff35db97303
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2024-04-29 16:21:28 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/party@685 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www_src/config.R
+++ www_src/config.R
@@ -3,6 +3,6 @@ pkg <- "party"
 dest <- "html"
 publish <- "../www"
 
-download.file("http://user.math.uzh.ch/hothorn/TH.bib", dest = "TH.bib")
+download.file("http://user.math.uzh.ch/hothorn/bib/TH.bib", dest = "TH.bib")
 system("cat party.bib >> TH.bib")
 bib <- "TH.bib"
