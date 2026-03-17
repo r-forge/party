a34b9cc636a02e11d06ef34b0014954f277cb699
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2026-03-17 11:37:15 +0000

    create devel branch
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/party@702 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkgdevel/R/internals.R
@@ -0,0 +1,9 @@
+
+party_intern <- function(..., fun = c("R_TreeGrow", "R_get_nodeID",
+    "R_getpredictions", "initVariableFrame", "ctreedpp", "newinputs",
+    "R_predict")) {
+
+    fun <- match.arg(fun)
+    fun <- gsub("^R", "\\.R", fun) ### needed to add . to R_xyz functions
+    do.call(fun, list(...))
+}
